2ef0583feca9ab9db196c8b8c557247c83a37a91
Rich Hagarty <rich.hagarty@ibm.com>
2017-07-13 09:41:32 -0700

    handle no games played and allow year to be configurable

--- config.new.js
+++ config.new.js
@@ -1,6 +1,9 @@
 // The attention word to wake up the robot.
 exports.attentionWord = 'watson';
 
+// MLB Season
+exports.MLBSeason = '2017';
+
 // You can change the voice of the robot to your favorite voice.
 exports.voice = 'en-US_MichaelVoice';
 // Some of the available options are:
