a53ad41f45e4e3ceb23507e2262aeb0dc0bff211
Rich Hagarty <rich.hagarty@ibm.com>
2017-09-29 14:45:37 -0700

    moved out saving function to new script

--- run.js
+++ run.js
@@ -1,4 +1,20 @@
-/* jshint esversion: 6 */
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+ /* jshint esversion: 6 */
 
 const WATSON = require('watson-developer-cloud');
 const CONFIG = require('./config.js');
@@ -24,23 +40,6 @@ var textPhoneNo = '';
 var context = {};
 
 var debug = false;
-
-/**
- * Only set 'saveData' to true if you wish to preserve the current MLB 
- * data during the actual season. This will write out data to the following 
- * files:
- * 
- * data/mlb-teams.json
- * data/mlb-standings.json
- * data/mlb-schedule.json
- * 
- * The master version of these files was a snapshot of the MLB season on 
- * 9/28/17. This data will allow users to run this app during the MLB 
- * off season (set 'config.offSeason' to true). Setting 'saveData' to true 
- * will overwrite these files and require that function getCurrentData()
- * be set appropriately.
- */
-var saveData = false;
 
 
 /**
@@ -154,15 +153,6 @@ function getMlbTeams() {
     REQUEST(options)
       .then(function (response) {
         mlbTeams = JSON.parse(response);
-
-        if (saveData) {
-          var jsonfile = require('jsonfile');
-          var file = 'data/mlb-teams.json';
-          jsonfile.writeFile(file, mlbTeams, {spaces: 2}, function (err) {
-            console.error(err);
-          });
-        }
-  
         return resolve();
       })
       .catch(function (err) {
@@ -190,15 +180,6 @@ function getMlbStandings() {
     REQUEST(options)
       .then(function (response) {
         mlbStandings = JSON.parse(response);
-
-        if (saveData) {
-          var jsonfile = require('jsonfile');
-          var file = 'data/mlb-standings.json';
-          jsonfile.writeFile(file, mlbStandings, {spaces: 2}, function (err) {
-            console.error(err);
-          });
-        }
-
         return resolve();
       })
       .catch(function (err) {
@@ -250,15 +231,6 @@ function getMlbSchedules() {
           }
           scheduleDaysCollected += 1;
           if (scheduleDaysCollected === 7) {
-
-            if (saveData) {
-              var jsonfile = require('jsonfile');
-              var file = 'data/mlb-schedule.json';
-              jsonfile.writeFile(file, mlbScheduleDates, {spaces: 2}, function (err) {
-                console.error(err);
-              });
-            }
-      
             return resolve();
           }
         })
